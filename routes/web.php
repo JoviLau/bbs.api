6dba2d430600b9270337c5fff96917e916c30c5f
Jovi <837717107@qq.com>
2019-03-23 14:50:41 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+//个人中心
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
