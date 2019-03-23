ee7e55cdba6e11bcd174eab51f5a72a3d78caec8
Jovi <837717107@qq.com>
2019-03-23 09:09:22 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="https://github.com/JoviLau/" target="_blank">Jovi</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:837717107@qq.com">联系我们</a></p>
+    </div>
+</footer>
