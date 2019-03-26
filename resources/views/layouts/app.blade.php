9bc4342302c18331fc434d9d23ec137f2aca2097
Jovi <837717107@qq.com>
2019-03-26 16:07:04 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'BBS Laravel') - Jovi</title>
+    <meta name="description" content="@yield('description', 'Jovi 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
