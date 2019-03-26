c5a389b249b59454aa205d184147e9dd48812352
Jovi <837717107@qq.com>
2019-03-26 15:56:27 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 //贴子
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 //分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
