6a42e9ddb7cd54a2dac0ab9f09cb04902589adfc
Jovi <837717107@qq.com>
2019-03-27 15:29:50 +0800

    用户最后活跃时间

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,9 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
 
         // 一小时执行一次『活跃用户』数据生成的命令
-        $schedule->command('larabbs:calculate-active-user')->hourly();
+        $schedule->command('laravel:calculate-active-user')->hourly();
+        // 每日零时执行一次
+        $schedule->command('laravel:sync-user-actived-at')->dailyAt('00:00');
     }
 
     /**
