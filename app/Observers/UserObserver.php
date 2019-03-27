bc2d6bbfb425b9ac0a93ee636aecb6b46234e5b7
Jovi <837717107@qq.com>
2019-03-27 15:42:50 +0800

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,6 +9,14 @@ use App\Models\User;
 
 class UserObserver
 {
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'http://pic.51yuansu.com/pic3/cover/01/69/80/595f67be8b8f7_610.jpg!/fw/260/quality/90/unsharp/true/compress/true';
+        }
+    }
+
     public function creating(User $user)
     {
         //
