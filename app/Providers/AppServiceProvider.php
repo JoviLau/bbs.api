8a2e000d06f71e7473912c8e880e12eb21903042
Jovi <837717107@qq.com>
2019-03-30 13:59:02 +0800

    topic delete api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
         if (app()->isLocal()) {
             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
         }
-        
+
         \API::error(function (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
             abort(404);
         });
