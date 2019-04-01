1da8440f220d550e8a397c376e8829e151f515ed
Jovi <837717107@qq.com>
2019-04-01 10:42:17 +0800

    weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,9 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'phone', 'email', 'password', 'introduction', 'avatar', 'weixin_openid', 'weixin_unionid', 'registration_id',
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'weixin_openid', 'weixin_unionid', 'registration_id',
+        'weixin_session_key', 'weapp_openid',
     ];
 
     /**
